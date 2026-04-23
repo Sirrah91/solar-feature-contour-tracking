ba9eb2fa028c4d19bee02fec51451ab4e458fdd5
Sirrah <92817370+Sirrah91@users.noreply.github.com>
2026-04-23 11:47:04 +0200

    Add files via upload

--- compile.sh
+++ compile.sh
@@ -15,13 +15,14 @@ mkdir -p "${dist_dir}"
 
 # --- List of Python scripts to compile ---
 scripts=(\
-    "job_sizes.py" \
-    "run_image_to_stats.py" \
-    "run_split_to_phases.py" \
-    "run_contour_tracking.py" \
-    "run_sunspot_association.py" \
-    "run_sunspot_tracking_multilevel.py" \
-    "run_calc_stats.py" \
+    #"job_sizes.py" \
+    #"run_image_to_stats.py" \
+    #"run_split_to_phases.py" \
+    #"run_contour_tracking.py" \
+    #"run_sunspot_association.py" \
+    #"run_sunspot_tracking_multilevel.py" \
+    #"run_calc_stats.py" \
+    "Bhor_prepare_data_plot_3d.py" \
     )
 
 cd "${src_dir}" || exit 1
