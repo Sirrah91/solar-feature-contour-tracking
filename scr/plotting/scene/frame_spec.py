fba4b3189f8b641a5ea0010f778066cbd001b4a5
Sirrah <92817370+Sirrah91@users.noreply.github.com>
2026-05-31 12:50:23 +0200

    Delete scr directory

--- scr/plotting/scene/frame_spec.py
+++
@@ -1,9 +0,0 @@
-from typing import NamedTuple
-
-from scr.utils.types_alias import ObservationID, FrameID
-
-
-class FrameSpec(NamedTuple):
-    observation_id: ObservationID
-    frame: FrameID
-    image_path: str
